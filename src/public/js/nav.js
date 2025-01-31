3e211cb9cf7b85887eeb7a0a30a45ea2f2cc3dd8
seblee666 <sebastianramos965@gmail.com>
2025-01-31 06:56:45 -0500

    toda la carpeta public

---
+++ src/public/js/nav.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+function toggleMenu() {
+    if (window.innerWidth <= 800) {
+      let menu = document.querySelector(".menu");
+      menu.classList.toggle("active");
+  
+      let toggle = document.querySelector(".toggle");
+      toggle.classList.toggle("active");
+    }
+  }
